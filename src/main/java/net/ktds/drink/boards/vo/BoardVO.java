2ed9dfa77a6d4b8ef6696e25e63c8834afbaf520
audghlrla <audghlrla1@gmail.com>
2016-10-11 09:11:18 +0900

    board수정

--- src/main/java/net/ktds/drink/boards/vo/BoardVO.java
+++ src/main/java/net/ktds/drink/boards/vo/BoardVO.java
@@ -1,5 +1,64 @@
 package net.ktds.drink.boards.vo;
 
 public class BoardVO {
-
+	private String boardId;
+	private String boardSubject;
+	private String boardContent;
+	private String userId;
+	private String createdDate;
+	private String fileName;
+	private String categoryId;
+	private String modyfyDate;
+	
+	
+	public String getBoardId() {
+		return boardId;
+	}
+	public void setBoardId(String boardId) {
+		this.boardId = boardId;
+	}
+	public String getBoardSubject() {
+		return boardSubject;
+	}
+	public void setBoardSubject(String boardSubject) {
+		this.boardSubject = boardSubject;
+	}
+	public String getBoardContent() {
+		return boardContent;
+	}
+	public void setBoardContent(String boardContent) {
+		this.boardContent = boardContent;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(String createdDate) {
+		this.createdDate = createdDate;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getModyfyDate() {
+		return modyfyDate;
+	}
+	public void setModyfyDate(String modyfyDate) {
+		this.modyfyDate = modyfyDate;
+	}
+	
+	
 }
